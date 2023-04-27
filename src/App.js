d417fddaf4c65e3fbcf6b572c7a954281ede2dee
Jovy Cortez <jovycortez11@gmail.com>
2023-04-26 22:35:54 -0400

    Lazy loading for About page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import { Home } from './components/Home'
-import { About } from './components/About'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Home } from './components/Home';
+//import { About } from './components/About';
 import { Navbar } from './components/Navbar';
 import { BookingSummary } from './components/BookingSummary';
 import { NoMatch } from './components/NoMatch';
 import { Services } from './components/Services';
 import { BridalPackages } from './components/BridalPackages';
 import { WeddingPartyPackages } from './components/WeddingPartyPackages';
+const LazyAbout = React.lazy(() => import('./components/About'));
 
 
 function App() {
@@ -18,7 +19,10 @@ function App() {
       </div>
       <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='about' element={<About />} />
+          <Route path='about' element={
+          <React.Suspense fallback='Loading...'>
+            <LazyAbout />
+          </React.Suspense>} />
           <Route path='booking-summary' element={<BookingSummary />} />
           <Route path='booking-summary' element={<BookingSummary />} />
           <Route path='services' element={<Services />}> 
--- src/components/BridalPackages.js
+++ src/components/BridalPackages.js
@@ -1,7 +1,10 @@
 import React from 'react'
 
+
 export const BridalPackages = () => {
   return (
-    <div>BridalPackages</div>
+    <div>BridalPackages
+      
+    </div>
   )
 }
